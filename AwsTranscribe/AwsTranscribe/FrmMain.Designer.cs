2d62c3eb271d20421e3e7665c6fbf71ff8aa8217
Matheus de Oliveira de Andrade <mathnhais@outlook.com>
2021-05-12 23:42:56 -0300

    desenvolvimento

---
+++ AwsTranscribe/AwsTranscribe/FrmMain.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace AwsTranscribe
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnRecord = new System.Windows.Forms.Button();
+            this.sfd = new System.Windows.Forms.SaveFileDialog();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnStop);
+            this.panel1.Controls.Add(this.btnRecord);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(968, 72);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Record your audio";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(244, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(116, 53);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnRecord
+            // 
+            this.btnRecord.Location = new System.Drawing.Point(122, 12);
+            this.btnRecord.Name = "btnRecord";
+            this.btnRecord.Size = new System.Drawing.Size(116, 53);
+            this.btnRecord.TabIndex = 0;
+            this.btnRecord.Text = "Record";
+            this.btnRecord.UseVisualStyleBackColor = true;
+            this.btnRecord.Click += new System.EventHandler(this.btnRecord_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.richTextBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 72);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(477, 657);
+            this.panel2.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(477, 657);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.richTextBox2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(477, 72);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(491, 657);
+            this.panel3.TabIndex = 2;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(491, 657);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(968, 729);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmMain";
+            this.Text = "AWS Transcribe";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnRecord;
+        private System.Windows.Forms.SaveFileDialog sfd;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ AwsTranscribe/AwsTranscribe/FrmMain.cs
@@ -0,0 +1,216 @@
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Transfer;
+using Amazon.TranscribeService;
+using Amazon.TranscribeService.Model;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AwsTranscribe
+{
+    public partial class FrmMain : Form
+    {
+        private const string AWS_PROFILE_NAME = "AWS Educate profile";
+        private const string AWS_S3_INPUT = "ia-transcribe-in";
+        private const string AWS_S3_OUTPUT = "ia-transcribe-out";
+
+        private AWSCredentials _awsCredentials;
+        private readonly RegionEndpoint _region = RegionEndpoint.USEast1;
+
+        AmazonTranscribeServiceClient _transcribeClient;
+
+        private string _fileName;
+
+        private string _jobInExecutionName;
+
+        // DLL Record voice
+        [DllImport("winmm.dll", EntryPoint = "mciSendStringA", ExactSpelling = true, CharSet = CharSet.Ansi, SetLastError = true)]
+        private static extern int mciSendString(string lpstrCommand, string lpstrReturnString, int uReturnLength, int hwndCallback);
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+            _fileName = Path.GetTempPath() + $"Record-{DateTime.Now.ToFileTime()}.wav";
+
+            mciSendString("open new Type waveaudio Alias recsound", "", 0, 0);
+            mciSendString("record recsound", "", 0, 0);
+            btnStop.Enabled = true;
+            btnRecord.Enabled = false;
+        }
+
+        private async void btnStop_Click(object sender, EventArgs e)
+        {
+            mciSendString("save recsound " + _fileName, "", 0, 0);
+            mciSendString("close recsound", "", 0, 0);
+
+            btnStop.Enabled = false;
+            btnRecord.Enabled = true;
+
+
+            GetCredentials();
+
+            await UploadToS3(_fileName);
+
+            await ExecuteTranscribe();
+        }
+
+        //private void btnOpen_Click(object sender, EventArgs e)
+        //{
+        //    SoundPlayer soundPlayer = new SoundPlayer(_fileName);
+        //    soundPlayer.Play();
+        //}
+
+        private async Task<bool> UploadToS3(string file)
+        {
+            try
+            {
+                AmazonS3Client s3Client = new AmazonS3Client(_awsCredentials, _region);
+                TransferUtility fileTransferUtility = new TransferUtility(s3Client);
+
+                await fileTransferUtility.UploadAsync(file, AWS_S3_INPUT);
+
+                return true;
+            }
+            catch (AmazonS3Exception ex)
+            {
+                MessageBox.Show($"Error encountered on server. Message: '{0}' when writing an object {ex.Message}");
+                return false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Unknown encountered on server. Message: '{0}' when writing an object {ex.Message}");
+                return false;
+            }
+        }
+
+        private async Task ExecuteTranscribe()
+        {
+            _transcribeClient = new AmazonTranscribeServiceClient(_awsCredentials, _region);
+
+            var transcriptionJobRequest = new StartTranscriptionJobRequest()
+            {
+                Media = new Media()
+                {
+                    MediaFileUri = $"s3://ia-transcribe-in/{Path.GetFileName(_fileName)}"
+                },
+                OutputBucketName = AWS_S3_OUTPUT,
+                LanguageCode = LanguageCode.PtBR,
+                MediaFormat = MediaFormat.Wav,
+                TranscriptionJobName = $"transcribe-custom-audio-{DateTime.Now.ToFileTime()}"
+            };
+
+            StartTranscriptionJobResponse jobResponse = await _transcribeClient.StartTranscriptionJobAsync(transcriptionJobRequest);
+
+            _jobInExecutionName = transcriptionJobRequest.TranscriptionJobName;
+
+            //new Task(() => { VerificarStatusJob(); }).Start();
+
+            new Thread(delegate ()
+            {
+                VerificarStatusJob();
+            }).Start();
+        }
+
+        private void GetCredentials()
+        {
+            var chain = new CredentialProfileStoreChain();
+
+            if (!chain.TryGetAWSCredentials(AWS_PROFILE_NAME, out _awsCredentials))
+            {
+                MessageBox.Show("Error on get credentials");
+            }
+        }
+
+        private void VerificarStatusJob()
+        {
+            if (string.IsNullOrEmpty(_jobInExecutionName))
+                return;
+
+            GetTranscriptionJobRequest jobStatus = new GetTranscriptionJobRequest
+            {
+                TranscriptionJobName = _jobInExecutionName,
+            };
+
+            GetTranscriptionJobResponse jobData = _transcribeClient.GetTranscriptionJobAsync(jobStatus).Result;
+
+            int contador = 0;
+
+            while (jobData.TranscriptionJob.TranscriptionJobStatus != TranscriptionJobStatus.COMPLETED)
+            {
+                Task.Delay(2000);
+
+                jobData = _transcribeClient.GetTranscriptionJobAsync(jobStatus).Result;
+
+                richTextBox1.BeginInvoke(new UpdateTextDelegate(UpdateText), $"Aguardando conclusão do job... Tentativa nº {contador}", false, null);
+                contador++;
+            }
+
+            string json = JsonConvert.SerializeObject(jobData.TranscriptionJob, Formatting.Indented);
+
+            richTextBox1.BeginInvoke(new UpdateTextDelegate(UpdateText), json, true, jobData.TranscriptionJob);
+        }
+
+        private void GetFileS3(string nameFile)
+        {
+            try
+            {
+                //AmazonS3Client s3Client = new AmazonS3Client(_awsCredentials, _region);
+
+                //TransferUtility fileTransferUtility = new TransferUtility(s3Client);                
+
+                //string caminhoArquivoBaixado = Path.GetTempPath();
+
+                //fileTransferUtility.Download(@"C:\Users\Matheus\", AWS_S3_OUTPUT, nameFile + ".json");                                                
+
+                //string arquivoLido = File.ReadAllText(caminhoArquivoBaixado + nameFile + ".json");                
+
+                //richTextBox2.Clear();
+                //richTextBox2.AppendText(arquivoLido);
+
+
+                richTextBox2.AppendText("Arquivo não lido");
+            }
+            catch (AmazonS3Exception ex)
+            {
+                MessageBox.Show($"Error encountered on server. Message: '{0}' when writing an object {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Unknown encountered on server. Message: '{0}' when writing an object {ex.Message}");
+            }
+        }
+
+        delegate void UpdateTextDelegate(string text, bool finished, TranscriptionJob transcriptionJob);
+        private void UpdateText(string text, bool finished, TranscriptionJob transcriptionJob)
+        {
+            if (richTextBox1.InvokeRequired)
+            {
+                richTextBox1.Clear();
+                richTextBox1.AppendText(text);
+            }
+            else
+            {
+                richTextBox1.Clear();
+                richTextBox1.AppendText(text);
+            }
+
+            if (finished)
+            {
+                GetFileS3(transcriptionJob.TranscriptionJobName);
+            }
+        }
+    }
+}
